9b347d2b696bff8f18a107f7fba086e4df9badab
glaucoacassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-20 00:04:24 -0300

    Create URI-2165.cpp

---
+++ URI-2165.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <string.h>
+#include <queue>
+#include <iomanip>
+#include <math.h>
+#include <utility>
+#include <sstream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+    string str;
+
+    getline(cin, str);
+    if(str.length() <= 140) cout<<"TWEET"<<endl;
+    else cout<<"MUTE"<<endl;
+    
+    return 0;
+}
