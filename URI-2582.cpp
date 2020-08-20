0f9cca18550f7a1d81fcc2dd5e047f420404353b
glaucoacassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-20 00:05:12 -0300

    Create URI-2582.cpp

---
+++ URI-2582.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <string.h>
+#include <queue>
+#include <iomanip>
+#include <math.h>
+#include <utility>
+#include <sstream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+    int c, a, b;
+    string v[12] = { "PROXYCITY", "P.Y.N.G.", "DNSUEY!", "SERVERS", "HOST!", "CRIPTONIZE", "OFFLINE DAY", "SALT", "ANSWER!", "RAR?", "WIFI ANTENNAS" };
+
+    cin>> c;
+    for (int i = 0; i < c; i++)
+    {
+        cin>> a >> b;
+        cout<< v[(a+b)] <<endl;   
+    }
+    return 0;
+}
