2975947c76a2759dd316fde2acf38d55157ae67d
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-01 16:32:28 -0300

    Create URI-1140.cpp

---
+++ AD-HOC/Flores Florescem da França/URI-1140.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#define fim '*'
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string str;
+
+	while (getline(cin, str) && str != "*")
+	{
+      fflush(stdin); //Limpar o buffer
+      bool istautograma = true;
+      for (size_t i = 0; i < str.length(); i++)
+      {
+        size_t pos = str.find(" ", i);
+        if (pos == string::npos)
+          break;
+        if (towlower(str[pos + 1]) != tolower(str[0]))
+        {
+          istautograma = false;
+          break;
+        }
+        i = pos; //atualiza a posicao
+      }
+      
+      if (istautograma) puts("Y");
+      else puts("N");
+	}
+	return 0;
+}
