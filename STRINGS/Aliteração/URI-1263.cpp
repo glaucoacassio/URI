072c163453354116412be6221e7b46fc3cfafeed
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-07 00:30:29 -0300

    Update URI-1263.cpp

--- STRINGS/Aliteração/URI-1263.cpp
+++ STRINGS/Aliteração/URI-1263.cpp
@@ -20,21 +20,20 @@ int32_t main() {
     while(getline(cin, str))
     {
         if(cin.eof()) break;
-        //Tem uma grande diferenca entre a funcao strcpy() e a variavel que criei. 
-        string str_cpy = "";
-        str_cpy = str_cpy + str[0];
+        string str_cp = "";
+        str_cp = str_cp + str[0];
         for (size_t i = 0; i<str.length(); i++)
         {
             size_t pos = str.find(" ", i);
             if (pos == string::npos) break;
-            str_cpy = str_cpy + str[pos + 1];
+            str_cp = str_cp + str[pos + 1];
             i = pos;
         }
 
         aliterations = 0;
-        for (int j = 1; j < str_cpy.length(); j++)
+        for (int j = 1; j < str_cp.length(); j++)
         {
-            if (tolower(str_cpy[j-1]) == tolower(str_cpy[j]) && tolower(str_cpy[j]) != tolower(str_cpy[j+1]))
+            if (tolower(str_cp[j-1]) == tolower(str_cp[j]) && tolower(str_cp[j]) != tolower(str_cp[j+1]))
                 aliterations++;
         }
         cout << aliterations << endl;
