ca2fdfdd69af7f5730dd99988cf4c04a06c9a19a
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-08 16:27:13 -0300

    Update URI-1199.cpp

--- MATEMATICA/Conversão Simples de Base/URI-1199.cpp
+++ MATEMATICA/Conversão Simples de Base/URI-1199.cpp
@@ -14,7 +14,7 @@ int main()
         //Returna um ponteiro pra um array que contem uma sequencia de caracteres representando o valor atual
         n = atoi(s.c_str());
         if(n < 0) return 0;
-        //Como todo numero hexadecimal seu segundo "digito" por assim dizer é sempre 'X'
+        //Como todo numero hexadecimal seu segundo "digito" por assim dizer é sempre 'x'
         //Simples verificacao...
         if(s[1] == 'x')
         {
