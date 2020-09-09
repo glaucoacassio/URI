41caf77edd4ee572a1d8749f15ad3c0a83d2159a
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-09 02:38:49 -0300

    Create URI-1069.cpp

---
+++ ESTRUTURAS E BIBLIOTECAS/Diamantes e Areia/URI-1069.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    string str;
+    int n;
+
+    cin >> n;
+    cin.ignore();
+    while (n--)
+    {
+        getline(cin, str);
+        int tmp = 0, diamonds = 0;
+        for (int i = 0; i < str.length(); ++i)
+        {
+            if (str[i] == '<') tmp++;
+            if (str[i] == '>' && tmp > 0)
+            {
+                diamonds++;
+                tmp--;
+            }
+        }
+        cout << diamonds << endl;
+    }
+    return 0;
+}
