c38aac433a35527d3454bb672f8afe170b2c1929
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-09 16:30:05 -0300

    Update URI-2516.cpp

--- MATEMATICA/Corrida/URI-2516.cpp
+++ MATEMATICA/Corrida/URI-2516.cpp
@@ -1,5 +1,4 @@
 /*
-File: 004.cpp
 
 Author: glaucoacassioc
 Created on 2020-09-09, 10:08:49
@@ -11,17 +10,17 @@ int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL) ; cout.tie(NULL) ;
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-    int s, va, vb;
+    int distancia, va, vb;
     double diferenca;
 
-    while (scanf("%d %d %d", &s, &va, &vb) != EOF)
+    while (scanf("%d %d %d", &distancia, &va, &vb) != EOF)
     {    
         if (va > vb) 
         {
-            diferenca = (double) s / (va-vb);
+            diferenca = (double) distancia / (va-vb);
             cout << fixed << setprecision(2) << diferenca << endl;
         }
-        else if (vb >= va || (va - vb) <= s) cout << "impossivel" << endl;
+        else if (vb >= va) cout << "impossivel" << endl;
     }
     return 0;
 }
